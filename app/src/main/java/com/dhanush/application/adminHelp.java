4d2f09b4c0f9c685b0cf09cee1ec43adf56f5a26
atdhansh <tulasidhanush22122000@gmail.com>
2021-12-07 00:03:10 +0530

    Intial Commit

---
+++ app/src/main/java/com/dhanush/application/adminHelp.java
@@ -0,0 +1,15 @@
+package com.dhanush.application;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class adminHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.admin_help);
+    }
+}
---
+++ app/src/main/java/com/dhanush/application/studentHelp.java
@@ -0,0 +1,14 @@
+package com.dhanush.application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class studentHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_help);
+    }
+}
